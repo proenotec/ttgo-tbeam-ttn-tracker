3bae30f899118afb2ad3521e1578bdf07079bff4
Manuel Castro <manuel.castro@proenotec.com>
2023-01-07 01:06:30 +0100

    second

--- main/credentials.h
+++ main/credentials.h
@@ -32,12 +32,12 @@ Credentials file
 
     // This should also be in little endian format (lsb), see above.
     // Note: You do not need to set this field, if unset it will be generated automatically based on the device macaddr
-    static u1_t DEVEUI[8]  = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    static u1_t DEVEUI[8]  = {0x91, 0x1D, 0x05, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
 
     // This key should be in big endian format (msb) (or, since it is not really a
     // number but a block of memory, endianness does not really apply). In
     // practice, a key taken from ttnctl can be copied as-is.
     // The key shown here is the semtech default key.
-    static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    static const u1_t PROGMEM APPKEY[16] = { 0xE8, 0xD0, 0x81, 0x39, 0xFE, 0x22, 0xD3, 0x1E, 0x42, 0x2C, 0x1B, 0x2F, 0xF6, 0x62, 0x83, 0x4B };
 
 #endif
